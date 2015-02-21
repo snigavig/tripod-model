79fb94c5fb72dcaf542b220a122f0ab39a6cbdbb
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2015-02-21 19:29:38 +0200

    quadrobot learns to move forward

--- bot.py
+++ bot.py
@@ -9,33 +9,35 @@ import numpy as np
 from math import sqrt, sin, cos
 from tools import get_random_item
 from random import seed
+import pygame as pg
 
 
 
 class Bot():
-	def __init__(self, body_width=3, leg_len=5, height=2, strand=1):
+	def __init__(self, body_width=10, leg_len=20, height=2, strand=1):
 		self.body = body_width
 		self.L = leg_len
 		self.H = height
 		self.S = strand
 		self.feet = np.zeros(4,dtype=np.int)
 		self.moves = np.array([
-			[-1,-1,-1,-1],
+			[-1,-1,1,1],
 			[ 4, 0, 0, 0],
 			[ 0, 4, 0, 0],
-			[ 0, 0, 4, 0],
-			[ 0, 0, 0, 4]
+			[ 0, 0,-4, 0],
+			[ 0, 0, 0,-4]
 			])
-		self.center = 0
+		self.center = np.array([0,0])
 		self.path = []
 
 		self.d_min = 0
-		self.d_max = sqrt(2*self.L**2 - self.H**2)
+		#self.d_max = sqrt(2*self.L**2 - self.H**2)
+		self.d_max = 7
 
 
 	def set_random_state(self):
 		seed()
-		Ls = range(8)
+		Ls = range(7)
 
 		self.feet = np.array([
 			get_random_item(Ls),
@@ -50,6 +52,7 @@ class Bot():
 		print "\nquadrobot info:\n"+"-"*25
 		print "  body size: (%i x %i)" %(self.body, self.body)
 		print "  leg length: (%i + %i)" % (self.L,self.L)
+		print "  feet range: (%i - %i)" % (int(self.d_min),int(self.d_max))
 		print "  initial feet position:"
 		print "  fore left: %i  right: %i" % tuple(self.feet[:2])
 		print "  hind left: %i  right: %i" % tuple(self.feet[2:])
@@ -70,7 +73,7 @@ class Bot():
 			self.path.append(action)
 			return True
 		else:
-			self.path.append(-action)
+			self.path.append(10+action)
 			return False
 
 
@@ -80,9 +83,28 @@ class Bot():
 		return sum([ self.feet[i]<<3*i for i in range(4)])
 
 
+	def draw(self, surf):
+		white = (250,250,250)
+		dark_grey = (50,50,50)
+		(x0,y0) = (100,100)
+		scale = 10
+		radius = scale * self.body / 10
+		
+		surf.fill(white)
+		rect = np.array([self.center[0]-self.body/2, self.center[1]-self.body/2, self.body, self.body])
+		pg.draw.rect(surf, dark_grey, tuple( rect*scale+np.array([x0,y0,0,0]) ),0)
+		
+		pos = np.array([self.center[0]+self.body/2, self.center[1]+self.body/2+self.feet[0]])
+		pg.draw.circle(surf, dark_grey, tuple( pos*scale+np.array([x0,y0]) ), radius, 0)
 
+		pos = np.array([self.center[0]-self.body/2, self.center[1]+self.body/2+self.feet[1]])
+		pg.draw.circle(surf, dark_grey, tuple( pos*scale+np.array([x0,y0]) ), radius, 0)
 
+		pos = np.array([self.center[0]+self.body/2, self.center[1]-self.body/2-self.feet[2]])
+		pg.draw.circle(surf, dark_grey, tuple( pos*scale+np.array([x0,y0]) ), radius, 0)
 
+		pos = np.array([self.center[0]-self.body/2, self.center[1]-self.body/2-self.feet[3]])
+		pg.draw.circle(surf, dark_grey, tuple( pos*scale+np.array([x0,y0]) ), radius, 0)
 
 
 
--- run.py
+++ run.py
@@ -9,7 +9,12 @@ import sys
 from bot import Bot
 from agent import Agent
 from tools import read_data, write_data
+import pygame as pg
+import numpy as np
+from time import sleep
 
+
+white = (250,250,250)
 
 
 def run(N):
@@ -23,21 +28,34 @@ def run(N):
 
 	for i in range(N):
 		bot = Bot()
-		run_episode(bot,agent,policy='eps_greedy', episode_len=20)
-		if bot.center > 7: print "quadrobot moved on: %i steps" % bot.center
+		run_episode(bot,agent, None, draw=False, policy='eps_greedy', episode_len=20)
+		#if bot.center[1] > 7: print "quadrobot moved on: %i steps" % bot.center[1]
+
 
 
 	_ = raw_input("Now see the demo. Press button to continue ...")
+	pg.init()
+	pg.display.init()
+	surf = pg.display.set_mode((300,600))
+	surf.fill(white)
+	pg.display.flip()
+	print "Surf1:", surf
+
+	
 	bot = Bot()
-	run_episode(bot,agent,policy='greedy',episode_len=50)
+	bot.info()
+	run_episode(bot,agent, surf, draw = True, policy='greedy', episode_len=50)
 	print "Robot moves:\n", bot.path
-	write_data(agent.Q,"data/spider_arr.dat")
+	write_data(agent.Q,"data/q.dat")
 	
 
-def run_episode(bot, agent, policy='random',episode_len=20):
+def run_episode(bot, agent, surf, draw=False, policy='random', episode_len=20):
 	bot.set_random_state()
-	
+	if draw: 
+		print "Bot feet:",bot.feet
+		
 	for _ in range(episode_len):
+
 		if policy == 'random':
 			action = agent.get_random_policy()
 		elif policy == 'greedy':
@@ -53,11 +71,17 @@ def run_episode(bot, agent, policy='random',episode_len=20):
 			reward = -5
 		elif action == 0: # body move forward
 			reward = 1
-			bot.center += 1
+			bot.center += np.array([0,1])
 		else:
 			reward = 0
 
 		agent.learn(prevState, bot.get_state(), action, reward)
+		
+		if draw: 
+			bot.draw(surf)
+			pg.display.flip()
+			sleep(0.2)
+			
 			
 
 
@@ -66,7 +90,9 @@ def run_episode(bot, agent, policy='random',episode_len=20):
 
 if __name__ == '__main__':
 	Args = sys.argv[:]
-	if len(Args) != 2:
+	if len(Args) !=2:
 		print "Wrong syntax\nUSAGE: python run.py <nbr_training_episodes>"
 	else:
 		run( int(Args[1]) )
+
+	
