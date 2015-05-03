6d9cf246d913056fa2d6f4dc435206ffb27c2bee
snigavig <snigavig@gmail.com>
2015-05-03 20:54:48 +0300

    Manual control v2

--- manual_control.py
+++ manual_control.py
@@ -1,127 +1,225 @@
-import serial
 import Tkinter
+from Tkconstants import *
+from Tkinter import Frame, Button, Entry, Label, StringVar
+import serial
+import threading
+
 
 __author__ = 'snigavig'
 
-PORT = '/dev/ttyACM0'
-SPEED = 9600
-top = Tkinter.Tk()
 
-
-def send_command(servonum):
+class ManualControl(Frame):
+    SERIAL_TIMEOUT = 100
+    PORT = '/dev/ttyACM0'
+    SPEED = 9600
+    DELIMITER = ","
+    current_state = [300, 320, 230, 430, 230, 430, 320, 430, 320] # dummy data
+    goal_state = []
+    entry_state_map = []
     connection = serial.Serial(PORT, SPEED, timeout=0, stopbits=serial.STOPBITS_TWO)
-    connection.write(str(servonum))
-    connection.close()
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.right_arm_horizontal_bottom_entry = Entry(self, bd=5)
+        self.right_arm_horizontal_top_entry = Entry(self, bd=5)
+        self.right_arm_vertical_entry = Entry(self, bd=5)
+        self.left_arm_horizontal_bottom_entry = Entry(self, bd=5)
+        self.left_arm_horizontal_top_entry = Entry(self, bd=5)
+        self.left_arm_vertical_entry = Entry(self, bd=5)
+        self.leg_horizontal_bottom_entry = Entry(self, bd=5)
+        self.leg_horizontal_top_entry = Entry(self, bd=5)
+        self.leg_vertical_entry = Entry(self, bd=5)
+        self.send_button = Button(self, text="Send command", command=self.send_command)
+
+        self.leg_vertical_label_variable = StringVar()
+        self.leg_horizontal_top_label_variable = StringVar()
+        self.leg_horizontal_bottom_label_variable = StringVar()
+        self.left_arm_vertical_label_variable = StringVar()
+        self.left_arm_horizontal_bottom_label_variable = StringVar()
+        self.right_arm_vertical_label_variable = StringVar()
+        self.right_arm_horizontal_top_label_variable = StringVar()
+        self.right_arm_horizontal_bottom_label_variable = StringVar()
+        self.left_arm_horizontal_top_label_variable = StringVar()
+
+        self.leg_vertical_label = Label(self, textvariable=self.leg_vertical_label_variable, text=self.current_state[8])
+        self.leg_horizontal_top_label = Label(self, textvariable=self.leg_horizontal_top_label_variable,
+                                              text=self.current_state[7])
+        self.leg_horizontal_bottom_label = Label(self, textvariable=self.leg_horizontal_bottom_label_variable,
+                                                 text=self.current_state[6])
+        self.left_arm_vertical_label = Label(self, textvariable=self.left_arm_vertical_label_variable,
+                                             text=self.current_state[5])
+        self.left_arm_horizontal_bottom_label = Label(self, textvariable=self.left_arm_horizontal_bottom_label_variable,
+                                                      text=self.current_state[3])
+        self.right_arm_vertical_label = Label(self, textvariable=self.right_arm_vertical_label_variable,
+                                              text=self.current_state[2])
+        self.right_arm_horizontal_top_label = Label(self, textvariable=self.right_arm_horizontal_top_label_variable,
+                                                    text=self.current_state[1])
+        self.right_arm_horizontal_bottom_label = Label(self,
+                                                       textvariable=self.right_arm_horizontal_bottom_label_variable,
+                                                       text=self.current_state[0])
+        self.left_arm_horizontal_top_label = Label(self, textvariable=self.left_arm_horizontal_top_label_variable,
+                                                   text=self.current_state[4])
+        self.parent = parent
+        self.init_ui()
+        self.read_serial()
+
+    def on_up_key_pressed_callback(self, widget):
+        self.goal_state[self.entry_state_map.index(widget)] += 10
+        widget.delete(0, END)
+        widget.insert(0, self.goal_state[self.entry_state_map.index(widget)])
+
+    def on_down_key_pressed_callback(self, widget):
+        self.goal_state[self.entry_state_map.index(widget)] -= 10
+        widget.delete(0, END)
+        widget.insert(0, self.goal_state[self.entry_state_map.index(widget)])
+
+    def read_ui_input(self):
+        self.goal_state = [
+            self.right_arm_horizontal_bottom_entry.get(),
+            self.right_arm_horizontal_top_entry.get(),
+            self.right_arm_vertical_entry.get(),
+            self.left_arm_horizontal_bottom_entry.get(),
+            self.left_arm_horizontal_top_entry.get(),
+            self.left_arm_vertical_entry.get(),
+            self.leg_horizontal_bottom_entry.get(),
+            self.leg_horizontal_top_entry.get(),
+            self.leg_vertical_entry.get()
+        ]
+
+    def send_command(self):
+        self.read_ui_input()
+        self.connection.write(str(self.DELIMITER.join(self.goal_state)))
+
+    def read_serial_mock(self):
+        print self.current_state
+
+    def read_serial(self):
+        if self.connection.inWaiting() > self.SERIAL_TIMEOUT:
+            self.current_state = self.connection.readline()
+            print self.current_state
+            self.current_state = self.current_state.rstrip().split(',')
+            self.update_ui_labels()
+            print self.current_state
 
 
-def back_vertical_bottom_plus_callback():
-    send_command(0)
+        self.parent.after(500, self.read_serial)
 
 
-def back_vertical_bottom_minus_callback():
-    send_command(0)
+    def update_ui_labels(self):
+        self.right_arm_horizontal_bottom_label_variable.set(self.current_state[0])
+        self.right_arm_horizontal_top_label_variable.set(self.current_state[1])
+        self.right_arm_vertical_label_variable.set(self.current_state[2])
+        self.left_arm_horizontal_bottom_label_variable.set(self.current_state[3])
+        self.left_arm_horizontal_top_label_variable.set(self.current_state[4])
+        self.left_arm_vertical_label_variable.set(self.current_state[5])
+        self.leg_horizontal_bottom_label_variable.set(self.current_state[6])
+        self.leg_horizontal_top_label_variable.set(self.current_state[7])
+        self.leg_vertical_label_variable.set(self.current_state[8])
+
+    def init_ui(self):
+
+        self.columnconfigure(0, pad=3)
+        self.columnconfigure(1, pad=3)
+
+        self.rowconfigure(0, pad=3)
+        self.rowconfigure(1, pad=3)
+        self.rowconfigure(2, pad=3)
+        self.rowconfigure(3, pad=3)
+        self.rowconfigure(4, pad=3)
+        self.rowconfigure(5, pad=3)
+        self.rowconfigure(6, pad=3)
+        self.rowconfigure(7, pad=3)
+        self.rowconfigure(8, pad=3)
+        self.rowconfigure(9, pad=3)
+
+        self.right_arm_horizontal_bottom_entry.bind('<Up>', lambda event: self.on_up_key_pressed_callback(
+            self.right_arm_horizontal_bottom_entry))
+        self.right_arm_horizontal_bottom_entry.bind('<Down>', lambda event: self.on_down_key_pressed_callback(
+            self.right_arm_horizontal_bottom_entry))
+        self.right_arm_horizontal_top_entry.bind('<Up>', lambda event: self.on_up_key_pressed_callback(
+            self.right_arm_horizontal_top_entry))
+        self.right_arm_horizontal_top_entry.bind('<Down>', lambda event: self.on_down_key_pressed_callback(
+            self.right_arm_horizontal_top_entry))
+        self.right_arm_vertical_entry.bind('<Up>', lambda event: self.on_up_key_pressed_callback(
+            self.right_arm_vertical_entry))
+        self.right_arm_vertical_entry.bind('<Down>', lambda event: self.on_down_key_pressed_callback(
+            self.right_arm_vertical_entry))
+        self.left_arm_horizontal_bottom_entry.bind('<Up>', lambda event: self.on_up_key_pressed_callback(
+            self.left_arm_horizontal_bottom_entry))
+        self.left_arm_horizontal_bottom_entry.bind('<Down>', lambda event: self.on_down_key_pressed_callback(
+            self.left_arm_horizontal_bottom_entry))
+        self.left_arm_horizontal_top_entry.bind('<Up>', lambda event: self.on_up_key_pressed_callback(
+            self.left_arm_horizontal_top_entry))
+        self.left_arm_horizontal_top_entry.bind('<Down>', lambda event: self.on_down_key_pressed_callback(
+            self.left_arm_horizontal_top_entry))
+        self.left_arm_vertical_entry.bind('<Up>', lambda event: self.on_up_key_pressed_callback(
+            self.left_arm_vertical_entry))
+        self.left_arm_vertical_entry.bind('<Down>', lambda event: self.on_down_key_pressed_callback(
+            self.left_arm_vertical_entry))
+        self.leg_horizontal_bottom_entry.bind('<Up>', lambda event: self.on_up_key_pressed_callback(
+            self.leg_horizontal_bottom_entry))
+        self.leg_horizontal_bottom_entry.bind('<Down>', lambda event: self.on_down_key_pressed_callback(
+            self.leg_horizontal_bottom_entry))
+        self.leg_horizontal_top_entry.bind('<Up>', lambda event: self.on_up_key_pressed_callback(
+            self.leg_horizontal_top_entry))
+        self.leg_horizontal_top_entry.bind('<Down>', lambda event: self.on_down_key_pressed_callback(
+            self.leg_horizontal_top_entry))
+        self.leg_vertical_entry.bind('<Up>',
+                                     lambda event: self.on_up_key_pressed_callback(self.leg_vertical_entry))
+        self.leg_vertical_entry.bind('<Down>',
+                                     lambda event: self.on_down_key_pressed_callback(self.leg_vertical_entry))
+
+        self.entry_state_map = [
+            self.right_arm_horizontal_bottom_entry,
+            self.right_arm_horizontal_top_entry,
+            self.right_arm_vertical_entry,
+            self.left_arm_horizontal_bottom_entry,
+            self.left_arm_horizontal_top_entry,
+            self.left_arm_vertical_entry,
+            self.leg_horizontal_bottom_entry,
+            self.leg_horizontal_top_entry,
+            self.leg_vertical_entry
+        ]
+
+        self.right_arm_horizontal_bottom_label.grid(row=0, column=0)
+        self.right_arm_horizontal_bottom_entry.grid(row=0, column=1)
+        self.right_arm_horizontal_top_label.grid(row=1, column=0)
+        self.right_arm_horizontal_top_entry.grid(row=1, column=1)
+        self.right_arm_vertical_label.grid(row=2, column=0)
+        self.right_arm_vertical_entry.grid(row=2, column=1)
+        self.left_arm_horizontal_bottom_label.grid(row=3, column=0)
+        self.left_arm_horizontal_bottom_entry.grid(row=3, column=1)
+        self.left_arm_horizontal_top_label.grid(row=4, column=0)
+        self.left_arm_horizontal_top_entry.grid(row=4, column=1)
+        self.left_arm_vertical_label.grid(row=5, column=0)
+        self.left_arm_vertical_entry.grid(row=5, column=1)
+        self.leg_horizontal_bottom_label.grid(row=6, column=0)
+        self.leg_horizontal_bottom_entry.grid(row=6, column=1)
+        self.leg_horizontal_top_label.grid(row=7, column=0)
+        self.leg_horizontal_top_entry.grid(row=7, column=1)
+        self.leg_vertical_label.grid(row=8, column=0)
+        self.leg_vertical_entry.grid(row=8, column=1)
+        self.send_button.grid(row=9, columnspan=2, sticky=W + E)
+
+        self.pack()
+
+        self.goal_state = self.current_state
+        self.right_arm_horizontal_bottom_entry.insert(0, self.goal_state[0])
+        self.right_arm_horizontal_top_entry.insert(0, self.goal_state[1])
+        self.right_arm_vertical_entry.insert(0, self.goal_state[2])
+        self.left_arm_horizontal_bottom_entry.insert(0, self.goal_state[3])
+        self.left_arm_horizontal_top_entry.insert(0, self.goal_state[4])
+        self.left_arm_vertical_entry.insert(0, self.goal_state[5])
+        self.leg_horizontal_bottom_entry.insert(0, self.goal_state[6])
+        self.leg_horizontal_top_entry.insert(0, self.goal_state[7])
+        self.leg_vertical_entry.insert(0, self.goal_state[8])
 
 
-def back_vertical_top_plus_callback():
-    send_command(1)
+def main():
+    root = Tkinter.Tk()
+    app = ManualControl(root)
+    root.mainloop()
 
 
-def back_vertical_top_minus_callback():
-    send_command(1)
-
-
-def back_horizontal_plus_callback():
-    send_command(14)
-
-
-def back_horizontal_minus_callback():
-    send_command(14)
-
-
-def left_vertical_bottom_plus_callback():
-    send_command(8)
-
-
-def left_vertical_bottom_minus_callback():
-    send_command(8)
-
-
-def left_vertical_top_plus_callback():
-    send_command(9)
-
-
-def left_vertical_top_minus_callback():
-    send_command(9)
-
-
-def left_horizontal_plus_callback():
-    send_command(10)
-
-
-def left_horizontal_minus_callback():
-    send_command(10)
-
-
-def right_vertical_bottom_plus_callback():
-    send_command(11)
-
-
-def right_vertical_bottom_minus_callback():
-    send_command(11)
-
-
-def right_vertical_top_plus_callback():
-    send_command(12)
-
-
-def right_vertical_top_minus_callback():
-    send_command(12)
-
-
-def right_horizontal_plus_callback():
-    send_command(13)
-
-
-def right_horizontal_minus_callback():
-    send_command(13)
-
-
-BBackVerticalBottomPlus = Tkinter.Button(top, text="BackVerticalBottom+", command=back_vertical_bottom_plus_callback)
-BBackVerticalBottomMinus = Tkinter.Button(top, text="BackVerticalBottom-", command=back_vertical_bottom_minus_callback)
-BBackVerticalTopPlus = Tkinter.Button(top, text="BackVerticalTop+", command=back_vertical_top_plus_callback)
-BBackVerticalTopMinus = Tkinter.Button(top, text="BackVerticalTop-", command=back_vertical_top_minus_callback)
-BBackHorizontalPlus = Tkinter.Button(top, text="BackHorizontal+", command=back_horizontal_plus_callback)
-BBackHorizontalMinus = Tkinter.Button(top, text="BackHorizontal-", command=back_horizontal_minus_callback)
-BLeftVerticalBottomPlus = Tkinter.Button(top, text="LeftVerticalBottom+", command=left_vertical_bottom_plus_callback)
-BLeftVerticalBottomMinus = Tkinter.Button(top, text="LeftVerticalBottom-", command=left_vertical_bottom_minus_callback)
-BLeftVerticalTopPlus = Tkinter.Button(top, text="LeftVerticalTop+", command=left_vertical_top_plus_callback)
-BLeftVerticalTopMinus = Tkinter.Button(top, text="LeftVerticalTop-", command=left_vertical_top_minus_callback)
-BLeftHorizontalPlus = Tkinter.Button(top, text="LeftHorizontal+", command=left_horizontal_plus_callback)
-BLeftHorizontalMinus = Tkinter.Button(top, text="LeftHorizontal-", command=left_horizontal_minus_callback)
-BRightVerticalBottomPlus = Tkinter.Button(top, text="RightVerticalBottom+", command=right_vertical_bottom_plus_callback)
-BRightVerticalBottomMinus = Tkinter.Button(top, text="RightVerticalBottom-", command=right_vertical_bottom_minus_callback)
-BRightVerticalTopPlus = Tkinter.Button(top, text="RightVerticalTop+", command=right_vertical_top_plus_callback)
-BRightVerticalTopMinus = Tkinter.Button(top, text="RightVerticalTop-", command=right_vertical_top_minus_callback)
-BRightHorizontalPlus = Tkinter.Button(top, text="RightHorizontal+", command=right_horizontal_plus_callback)
-BRightHorizontalMinus = Tkinter.Button(top, text="RightHorizontal-", command=right_horizontal_minus_callback)
-
-BBackVerticalBottomPlus.pack()
-BBackVerticalBottomMinus.pack()
-BBackVerticalTopPlus.pack()
-BBackVerticalTopMinus.pack()
-BBackHorizontalPlus.pack()
-BBackHorizontalMinus.pack()
-BLeftVerticalBottomPlus.pack()
-BLeftVerticalBottomMinus.pack()
-BLeftVerticalTopPlus.pack()
-BLeftVerticalTopMinus.pack()
-BLeftHorizontalPlus.pack()
-BLeftHorizontalMinus.pack()
-BRightVerticalBottomPlus.pack()
-BRightVerticalBottomMinus.pack()
-BRightVerticalTopPlus.pack()
-BRightVerticalTopMinus.pack()
-BRightHorizontalPlus.pack()
-BRightHorizontalMinus.pack()
-
-top.mainloop()
+if __name__ == '__main__':
+    main()
--- motion/motion.ino
+++ motion/motion.ino
@@ -7,114 +7,162 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 #define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
-#define BACKVERTICALBOTTOM 0
-#define BACKVERTICALTOP 1
-#define BACKHORIZONTAL 14
-#define LEFTVERTICALBOTTOM 8
-#define LEFTVERTICALTOP 9
-#define LEFTHORIZONTAL 10
 #define RIGHTVERTICALBOTTOM 11
 #define RIGHTVERTICALTOP 12
 #define RIGHTHORIZONTAL 13
+#define LEFTVERTICALBOTTOM 8
+#define LEFTVERTICALTOP 9
+#define LEFTHORIZONTAL 10
+#define BACKVERTICALBOTTOM 0
+#define BACKVERTICALTOP 1
+#define BACKHORIZONTAL 14
 #define N (3)
  
 int CurrentState[N][N];
 int GoalState[N][N];
+int DifferenceBetweenStates[N][N];
+int Stand1State[N][N];
 
 //initial position
-int LeftVerticalBottom = 450;
-int LeftVerticalTop = 400;
-int LeftHorizontal = 350;
-int RightVerticalBottom = 350; 
-int RightVerticalTop = 350;
+int RightVerticalBottom = 380; 
+int RightVerticalTop = 400;
 int RightHorizontal = 350;
-int BackVerticalBottom = 300;
-int BackVerticalTop = 350;
+int LeftVerticalBottom = 250;
+int LeftVerticalTop = 350;
+int LeftHorizontal = 350;
+int BackVerticalBottom = 420;
+int BackVerticalTop = 390;
 int BackHorizontal = 400;
+
+//jump position
+//int RightVerticalBottom = 380; 
+//int RightVerticalTop = 400;
+//int RightHorizontal = 350;
+//int LeftVerticalBottom = 400;
+//int LeftVerticalTop = 350;
+//int LeftHorizontal = 350;
+//int BackVerticalBottom = 470;
+//int BackVerticalTop = 480;
+//int BackHorizontal = 400;
 
 void setup() {
   Serial.begin(9600);
   pwm.begin();
   pwm.setPWMFreq(60);
-  CurrentState[0][0] = LeftVerticalBottom;
-  CurrentState[0][1] = LeftVerticalTop;
-  CurrentState[0][2] = LeftHorizontal;
-  CurrentState[1][0] = RightVerticalBottom;
-  CurrentState[1][1] = RightVerticalTop;
-  CurrentState[1][2] = RightHorizontal;
-  CurrentState[2][0] = BackVerticalBottom;
-  CurrentState[2][1] = BackVerticalTop;
-  CurrentState[2][2] = BackHorizontal;
+  Stand1State[0][0] = RightVerticalBottom;
+  Stand1State[0][1] = RightVerticalTop;
+  Stand1State[0][2] = RightHorizontal;
+  Stand1State[1][0] = LeftVerticalBottom;
+  Stand1State[1][1] = LeftVerticalTop;
+  Stand1State[1][2] = LeftHorizontal;
+  Stand1State[2][0] = BackVerticalBottom;
+  Stand1State[2][1] = BackVerticalTop;
+  Stand1State[2][2] = BackHorizontal;
+  move_to_stand_1();
+  delay(500);
 }
 
+//////////////////////////// ~ M A I N ~ L O O P ~ ////////////////////////////
 void loop() {
   String content = "";
   if (Serial.available()) {
     while (Serial.available()) {
-      content += Serial.read();
+      content += Serial.readString();
     }
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        int index = content.indexOf(","); //We find the next comma
+        GoalState[i][j] = atol(content.substring(0,index).c_str()); //Extract the number
+        content = content.substring(index+1); //Remove the number from the string
+      }
+    }
+    
+    calculate_difference();
+    move_to_goal_state();
   }
+  
+  Serial.println(current_state_to_string());
+  
+  delay(500);
+}
+//////////////////////////// ~ M A I N ~ L O O P ~ END ~ ////////////////////////////
 
+String current_state_to_string() {
+  String result;
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
-      int index = content.indexOf(","); //We find the next comma
-      GoalState[i][j] = atol(content.substring(0,index).c_str()); //Extract the number
-      content = content.substring(index+1); //Remove the number from the string
+      result += CurrentState[i][j];
+      if (!(i==2 && j==2)) {
+        result += ",";
+      }
+    }
+  }
+  return result;
+}  
+
+String goal_state_to_string() {
+  String result;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      result += GoalState[i][j];
+      if (!(i==2 && j==2)) {
+        result += ",";
+      }
+    }
+  }
+  return result;
+} 
+
+void calculate_difference() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      DifferenceBetweenStates[i][j] = CurrentState[i][j] - GoalState[i][j];
     }
   }
 }
-  
-//  pwm.setPWM(0, 0, BackVerticalBottom);
-//  pwm.setPWM(1, 0, BackVerticalTop);
-//  pwm.setPWM(14, 0, BackHorizontal);
-//  pwm.setPWM(8, 0, LeftVerticalBottom);
-//  pwm.setPWM(9, 0, LeftVerticalTop);
-//  pwm.setPWM(10, 0, LeftHorizontal);
-//  pwm.setPWM(11, 0, RightVerticalBottom);
-//  pwm.setPWM(12, 0, RightVerticalTop);
-//  pwm.setPWM(13, 0, RightHorizontal);
-//  if (Serial.available() > 0) {
-//    int command = Serial.read();
-//    
-//    switch (command - 48) { //this was fun
-//    case BACKVERTICALBOTTOM:
-//      BackVerticalBottom = BackVerticalBottom + 10;
-//      pwm.setPWM(0, 0, BackVerticalBottom);
-//      break;
-//    case BACKVERTICALTOP:
-//      BackVerticalTop = BackVerticalTop + 10;
-//      pwm.setPWM(1, 0, BackVerticalTop);
-//      break;
-//    case BACKHORIZONTAL:
-//      BackHorizontal = BackHorizontal + 10;
-//      pwm.setPWM(14, 0, BackHorizontal);
-//      break;
-//    case LEFTVERTICALBOTTOM:
-//      LeftVerticalBottom = LeftVerticalBottom + 10;
-//      pwm.setPWM(8, 0, LeftVerticalBottom);
-//      break;
-//    case LEFTVERTICALTOP:
-//      LeftVerticalTop = LeftVerticalTop + 10;
-//      pwm.setPWM(9, 0, LeftVerticalTop);
-//      break;
-//    case LEFTHORIZONTAL:
-//      LeftHorizontal = LeftHorizontal + 10;
-//      pwm.setPWM(10, 0, LeftHorizontal);
-//      break;
-//    case RIGHTVERTICALBOTTOM:
-//      RightVerticalBottom = RightVerticalBottom + 10;
-//      pwm.setPWM(11, 0, RightVerticalBottom);
-//      break;
-//    case RIGHTVERTICALTOP:
-//      RightVerticalTop = RightVerticalTop + 10;
-//      pwm.setPWM(12, 0, RightVerticalTop);
-//      break;
-//    case RIGHTHORIZONTAL:
-//      RightHorizontal = RightHorizontal + 10;
-//      pwm.setPWM(13, 0, RightHorizontal);
-//      break;
-//    default: 
-//      Serial.println("Only 0-15 are supported.");
-//    }
-//    Serial.println(command);           // to the serial monitor
-//  }
+
+void move_to_goal_state() {
+  move_servo(RIGHTVERTICALBOTTOM, GoalState[0][0]);
+  move_servo(RIGHTVERTICALTOP, GoalState[0][1]);
+  move_servo(RIGHTHORIZONTAL, GoalState[0][2]);
+  move_servo(LEFTVERTICALBOTTOM, GoalState[1][0]);
+  move_servo(LEFTVERTICALTOP, GoalState[1][1]);
+  move_servo(LEFTHORIZONTAL, GoalState[1][2]);
+  move_servo(BACKVERTICALBOTTOM, GoalState[2][0]);
+  move_servo(BACKVERTICALTOP, GoalState[2][1]);
+  move_servo(BACKHORIZONTAL, GoalState[2][2]);
+  CurrentState[0][0] = GoalState[0][0];
+  CurrentState[0][1] = GoalState[0][1];
+  CurrentState[0][2] = GoalState[0][2];
+  CurrentState[1][0] = GoalState[1][0];
+  CurrentState[1][1] = GoalState[1][1];
+  CurrentState[1][2] = GoalState[1][2];
+  CurrentState[2][0] = GoalState[2][0];
+  CurrentState[2][1] = GoalState[2][1];
+  CurrentState[2][2] = GoalState[2][2];
+}
+
+void move_to_stand_1() {
+  move_servo(RIGHTVERTICALBOTTOM, Stand1State[0][0]);
+  move_servo(RIGHTVERTICALTOP, Stand1State[0][1]);
+  move_servo(RIGHTHORIZONTAL, Stand1State[0][2]);
+  move_servo(LEFTVERTICALBOTTOM, Stand1State[1][0]);
+  move_servo(LEFTVERTICALTOP, Stand1State[1][1]);
+  move_servo(LEFTHORIZONTAL, Stand1State[1][2]);
+  move_servo(BACKVERTICALBOTTOM, Stand1State[2][0]);
+  move_servo(BACKVERTICALTOP, Stand1State[2][1]);
+  move_servo(BACKHORIZONTAL, Stand1State[2][2]);
+  CurrentState[0][0] = Stand1State[0][0];
+  CurrentState[0][1] = Stand1State[0][1];
+  CurrentState[0][2] = Stand1State[0][2];
+  CurrentState[1][0] = Stand1State[1][0];
+  CurrentState[1][1] = Stand1State[1][1];
+  CurrentState[1][2] = Stand1State[1][2];
+  CurrentState[2][0] = Stand1State[2][0];
+  CurrentState[2][1] = Stand1State[2][1];
+  CurrentState[2][2] = Stand1State[2][2];
+}
+
+void move_servo(int servo_id, int value) {
+  pwm.setPWM(servo_id, 0 , value);
+}
